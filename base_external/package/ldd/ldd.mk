4a99cc40f77a280c61112871c6a915d741c3619a
hampter <hampter@Ubuntu.myguest.virtualbox.org>
2025-03-07 10:21:37 -0700

    correction to loading of ldd

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -5,6 +5,12 @@ AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
 LDD_MODULE_SUBDIRS = misc-modules scull
 
+define LDD_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 0755 $(@D)/misc-modules/hello.ko $(TARGET_DIR)/lib/modules/
+	$(INSTALL) -D -m 0755 $(@D)/misc-modules/faulty.ko $(TARGET_DIR)/lib/modules/
+	$(INSTALL) -D -m 0755 $(@D)/scull/scull.ko $(TARGET_DIR)/lib/modules/
+endef
+
 $(eval $(kernel-module))
 $(eval $(generic-package))
 
