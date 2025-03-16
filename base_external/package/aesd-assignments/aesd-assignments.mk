daa519e50fca6adc016e461015cf09afdb52a29b
hampter <hampter@Ubuntu.myguest.virtualbox.org>
2025-03-15 20:26:12 -0600

    troubleshooting of aesd dev param not being passed

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -5,7 +5,7 @@
 #
 ##############################################################
 
-AESD_ASSIGNMENTS_VERSION = '36d4909c51e96ad856fed8d5f440f1e90559c2ee'
+AESD_ASSIGNMENTS_VERSION = '649b9f95f834fa6b70695d262c9398d27e34fd95'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -13,9 +13,15 @@ AESD_ASSIGNMENTS_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-erp
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
+USE_AESD_CHAR_DEVICE ?= 1
+
 define AESD_ASSIGNMENTS_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/server USE_AESD_CHAR_DEVICE=1
+	$(info Executing make on server with USE_AESD_CHAR_DEVICE set to $(USE_AESD_CHAR_DEVICE))
+	# I had some issues making sure the make runs with USE_AESD_CHAR_DEVICE=1...
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/server CFLAGS="-Wall -Werror -DUSE_AESD_CHAR_DEVICE=1" V=1
+	strings $(@D)/server/aesdsocket | grep "aesdsocket configured"
+	$(info Completed make on server with USE_AESD_CHAR_DEVICE set to $(USE_AESD_CHAR_DEVICE))
 	$(MAKE) -C $(LINUX_DIR) M=$(@D)/aesd-char-driver ARCH=$(KERNEL_ARCH) CROSS_COMPILE=$(TARGET_CROSS) modules
 endef
 
